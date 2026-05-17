b02e2e4d8886e39108fcdc57e43f50846a3c229a
Joe Jenkinson <39272311+ukjinx@users.noreply.github.com>
2026-05-17 21:00:38 +0100

    Various improvements

--- script.js
+++ script.js
@@ -1,3 +1,49 @@
+async function loadComponent(id, file) {
+
+  const element = document.getElementById(id);
+
+  if (!element) return;
+
+  const response = await fetch(file);
+
+  const html = await response.text();
+
+  element.innerHTML = html;
+
+}
+
+loadComponent(
+  "nav-placeholder",
+  "components/nav.html"
+);
+
+loadComponent(
+  "footer-placeholder",
+  "components/footer.html"
+);
+
+window.addEventListener("DOMContentLoaded", () => {
+
+  const currentPage =
+    window.location.pathname.split("/").pop();
+
+  const navLinks =
+    document.querySelectorAll(".topnav a");
+
+  navLinks.forEach(link => {
+
+    const href = link.getAttribute("href");
+
+    if (href === currentPage) {
+
+      link.classList.add("active");
+
+    }
+
+  });
+
+});
+
 const sections =
   document.querySelectorAll(".about-section");
 
